97d50f74af7d0305f5f3161798648e6ccf25d1a2
Christopher Schleiden <cschleiden@github.com>
2023-01-10 16:05:50 -0800

    Clean up transform function

--- actions-languageservice/src/utils/transform.ts
+++ actions-languageservice/src/utils/transform.ts
@@ -1,7 +1,7 @@
 import {Position, TextDocument} from "vscode-languageserver-textdocument";
 import {Range} from "vscode-languageserver-types";
 
-const DUMMY_KEY = "dummy";
+const PLACEHOLDER_KEY = "key";
 
 // Transform a document to work around YAML parsing issues
 // Based on `_transform` in https://github.com/cschleiden/github-actions-parser/blob/main/src/lib/parser/complete.ts#L311
@@ -27,29 +27,33 @@ export function transform(doc: TextDocument, pos: Position): [TextDocument, Posi
   // Special case for Actions, if this line contains an expression marker, do _not_ transform. This is
   // an ugly fix for auto-completion in multi-line YAML strings. At this point in the process, we cannot
   // determine if a line is in such a multi-line string.
-  if (line.indexOf("${{") === -1) {
-    const colon = line.indexOf(":");
-    if (colon === -1) {
-      const trimmedLine = line.trim();
-      if (trimmedLine === "" || trimmedLine === "-") {
-        // Node in sequence or empty line
-        let spacer = "";
-        if (trimmedLine === "-" && !line.endsWith(" ")) {
-          spacer = " ";
-          offset++;
-        }
+  if (line.indexOf("${{") !== -1) {
+    return [doc, pos];
+  }
 
-        line =
-          line.substring(0, linePos) + spacer + DUMMY_KEY + (trimmedLine === "-" ? "" : ":") + line.substring(linePos);
-
-        // Adjust pos by one to prevent a sequence node being marked as active
+  const containsColon = line.indexOf(":") !== -1;
+  if (!containsColon) {
+    const trimmedLine = line.trim();
+    if (trimmedLine === "" || trimmedLine === "-") {
+      // Pos in sequence or empty line
+      let spacer = "";
+      if (trimmedLine === "-" && !line.endsWith(" ")) {
+        spacer = " ";
         offset++;
-      } else if (!trimmedLine.startsWith("-")) {
-        // Add `:` to end of line
-        line = line + ":";
       }
-    } else {
-      offset = offset - 1;
+
+      line =
+        line.substring(0, linePos) +
+        spacer +
+        PLACEHOLDER_KEY +
+        (trimmedLine === "-" ? "" : ":") +
+        line.substring(linePos);
+
+      // Adjust pos by one to prevent a sequence node being marked as active
+      offset++;
+    } else if (!trimmedLine.startsWith("-")) {
+      // Add `:` to end of line
+      line = line + ":";
     }
   }
 
