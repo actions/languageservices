c4878356a280c3b987291d17bf90a9988b9cf110
Christopher Schleiden <cschleiden@github.com>
2023-01-24 17:39:24 -0800

    Add comments to the initalization options

--- actions-languageserver/src/initializationOptions.ts
+++ actions-languageserver/src/initializationOptions.ts
@@ -2,17 +2,42 @@ import {LogLevel} from "@github/actions-languageservice/log";
 export {LogLevel} from "@github/actions-languageservice/log";
 
 export interface InitializationOptions {
+  /**
+   * GitHub token that will be used to retrieve additional information from github.com
+   *
+   * Requires the `repo` and `workflow` scopes
+   */
   sessionToken?: string;
 
+  /**
+   * List of repositories that the language server should be aware of
+   */
   repos?: RepositoryContext[];
 
+  /**
+   * Desired log level
+   */
   logLevel?: LogLevel;
 }
 
 export interface RepositoryContext {
+  /**
+   * Repository ID
+   */
   id: number;
+
+  /**
+   * Repository owner
+   */
   owner: string;
+
+  /**
+   * Repository name
+   */
   name: string;
 
+  /**
+   * Local workspace uri
+   */
   workspaceUri: string;
 }
