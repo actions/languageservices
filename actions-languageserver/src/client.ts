7baf5da7eed34f4e9e88fb35d8056a30408d81a7
Christopher Schleiden <cschleiden@github.com>
2023-01-31 09:13:44 -0800

    Remove package.json version import

--- actions-languageserver/src/client.ts
+++ actions-languageserver/src/client.ts
@@ -1,9 +1,8 @@
 import {Octokit} from "@octokit/rest";
-import {version} from "../package.json";
 
 export function getClient(token: string, userAgent?: string): Octokit {
   return new Octokit({
     auth: token,
-    userAgent: userAgent || `GitHub Actions Language Server (${version})`
+    userAgent: userAgent || `GitHub Actions Language Server`
   });
 }
