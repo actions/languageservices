b516b65d3e0277e8b9e3dd8d7999cf77399b93f7
Christopher Schleiden <cschleiden@github.com>
2023-03-06 11:38:44 -0800

    Move action url generation to shared function

--- languageservice/src/action.ts
+++ languageservice/src/action.ts
@@ -65,3 +65,10 @@ export function actionIdentifier(ref: ActionReference): string {
   }
   return `${ref.owner}/${ref.name}/${ref.ref}`;
 }
+
+export function actionUrl(actionRef: ActionReference): string {
+  // TODO: Support base uri for GHES
+  const gitHubBaseUri = "https://www.github.com/";
+
+  return `${gitHubBaseUri}${actionRef.owner}/${actionRef.name}/tree/${actionRef.ref}/${actionRef.path || ""}`;
+}
