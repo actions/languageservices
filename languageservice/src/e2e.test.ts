d1baf65c180f19f65147bc69aad98c267984d9a4
Christopher Schleiden <cschleiden@live.de>
2023-03-21 11:15:00 -0700

    Add failing e2e test

---
+++ languageservice/src/e2e.test.ts
@@ -0,0 +1,28 @@
+import {complete} from "./complete";
+import {hover} from "./hover";
+import {registerLogger} from "./log";
+import {getPositionFromCursor} from "./test-utils/cursor-position";
+import {TestLogger} from "./test-utils/logger";
+import {clearCache} from "./utils/workflow-cache";
+
+registerLogger(new TestLogger());
+
+beforeEach(() => {
+  clearCache();
+});
+
+describe("end-to-end", () => {
+  it("empty workflow completion after hover", async () => {
+    const input = "|";
+
+    // Issue hover first to fill the cache
+    await hover(...getPositionFromCursor(input));
+
+    const result = await complete(...getPositionFromCursor(input));
+
+    expect(result).not.toBeUndefined();
+    expect(result.length).toEqual(8);
+    const labels = result.map(x => x.label);
+    expect(labels).toEqual(["concurrency", "defaults", "env", "jobs", "name", "on", "permissions", "run-name"]);
+  });
+});
