11d3fc25ee614dd7003861e0d8049d2bee9219dd
Jonathan Tamsut <jtamsut@users.noreply.github.com>
2023-04-13 15:26:31 -0700

    run prettier

--- languageservice/src/complete.test.ts
+++ languageservice/src/complete.test.ts
@@ -480,7 +480,9 @@ jobs:
 
     const result = await complete(...getPositionFromCursor(input));
 
-    expect(result.filter(x => x.label === "cancel-in-progress").map(x => x.textEdit?.newText)).toEqual(["\n  cancel-in-progress: "])
-    expect(result.filter(x => x.label === "group").map(x => x.textEdit?.newText)).toEqual(["\n  group: "])
-  })
+    expect(result.filter(x => x.label === "cancel-in-progress").map(x => x.textEdit?.newText)).toEqual([
+      "\n  cancel-in-progress: "
+    ]);
+    expect(result.filter(x => x.label === "group").map(x => x.textEdit?.newText)).toEqual(["\n  group: "]);
+  });
 });
