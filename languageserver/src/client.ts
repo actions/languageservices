484b6eb423c1c7f03c8e50e762fdeb7674d3cdda
Crystal Tenn <crtenn@microsoft.com>
2023-04-19 16:36:59 -0400

    add insecure_ssl set to 1 to reduce security checks for certificates

--- languageserver/src/client.ts
+++ languageserver/src/client.ts
@@ -3,6 +3,9 @@ import {Octokit} from "@octokit/rest";
 export function getClient(token: string, userAgent?: string): Octokit {
   return new Octokit({
     auth: token,
-    userAgent: userAgent || `GitHub Actions Language Server`
+    userAgent: userAgent || `GitHub Actions Language Server`,
+    request: {
+      insecure_ssl: 1
+    }
   });
 }
