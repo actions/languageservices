57a5994e12f0f38610114570e750bf07672db0a9
Jonathan Tamsut <jtamsut@users.noreply.github.com>
2023-05-01 14:51:54 -0700

    add the ability to read in cert

--- languageserver/src/client.ts
+++ languageserver/src/client.ts
@@ -1,8 +1,28 @@
 import {Octokit} from "@octokit/rest";
+import { Agent } from "node:https";
+import { readFileSync } from "fs";
 
 export function getClient(token: string, userAgent?: string): Octokit {
-  return new Octokit({
-    auth: token,
-    userAgent: userAgent || `GitHub Actions Language Server`
-  });
+  const selfSignedCertPath = process.env.NODE_EXTRA_CA_CERTS;
+
+  // if NODE_EXTRA_CA_CERTS is set then use the self-signed cert to make the request
+  if (selfSignedCertPath) {
+    const httpsAgent = new Agent({
+      ca: readFileSync(selfSignedCertPath)
+    });
+
+    return new Octokit({
+      auth: token,
+      userAgent: userAgent || `GitHub Actions Language Server`,
+      request: {
+        agent: httpsAgent
+      },
+    });
+
+  } else {
+    return new Octokit({
+      auth: token,
+      userAgent: userAgent || `GitHub Actions Language Server`
+    });
+  }
 }
