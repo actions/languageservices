57a77551b0f9e0d3140577aadb0865479d84738b
flmeyer <florian.meyer@capgemini.com>
2023-05-08 17:00:05 +0200

    Enable support for GitHub Enterprise Server

--- languageserver/src/client.ts
+++ languageserver/src/client.ts
@@ -1,8 +1,9 @@
 import {Octokit} from "@octokit/rest";
 
-export function getClient(token: string, userAgent?: string): Octokit {
+export function getClient(token: string, userAgent?: string, apiUrl?: string): Octokit {
   return new Octokit({
     auth: token,
-    userAgent: userAgent || `GitHub Actions Language Server`
+    userAgent: userAgent || `GitHub Actions Language Server`,
+    baseUrl: apiUrl
   });
 }
